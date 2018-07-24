4fcf9ed0ac489ec996e26fc7bb9223630fd571aa
kayvan73 <kayvan73@protonmail.com>
2018-07-24 16:49:53 -0500

    a) added new database + tables to the repo (using samba) b) started implementing image-file storage into existing code, running into errors

--- ActianUI/dataCollection/py2_data/TabVar_access_py2.py
+++ ActianUI/dataCollection/py2_data/TabVar_access_py2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python2.7
+import io
+from PIL import Image
 import os
 import sys
 from datetime import datetime
@@ -8,14 +10,14 @@ print(os.getcwd())
 sys.path.insert(1, '../../swigFiles/swigFiles_py2')  #need these to talk to btrieve2
 import btrievePython as btrv
 os.chdir('../../PSQL')
-btrieveFileName = 'TargetMatches.mkd'
-recordFormat = '<iB40sBdBdBBBBB'
+btrieveFileName = 'BigBoi.mkd'
+recordFormat = '<iB8sBdBd'
 #i = integer (for the IDENTITY, 4bytes)
 #B = null byte(goes in between columns, 1byte)
 #d = double (8bytes)
 #s = char
 #BBBB = for the TIME datatype byte for sec,time,hour,PM/AM
-recordLength = 68
+recordLength = 31
 keyFormat = '<i'
 
 
@@ -54,33 +56,65 @@ else:
 def select_all():
     selectALL = []
     #recordFormat = '<iBs50BdBdBBBBB'
-    record = struct.pack(recordFormat, 0, 0, ''.ljust(40).encode('UTF-8'), 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    record = struct.pack(recordFormat, 0, 0, bytes(8), 0, 0, 0, 0)
     readLength = btrieveFile.RecordRetrieveFirst(btrv.Btrieve.INDEX_1, record, 0)
     print(readLength)
     while (readLength > 0):
         humanReadable_record = (struct.unpack(recordFormat, record))
         readLength = btrieveFile.RecordRetrieveNext(record, 0)
         selectALL.append(humanReadable_record)
+    #closeTable()
     return (selectALL)
 
 
 def get_last():
-    record = struct.pack(recordFormat,  0, 0, ''.ljust(40).encode('UTF-8'), 0, 0, 0, 0, 0, 0, 0, 0, 0)
-    readLength = btrieveFile.RecordRetrieveLast(btrv.Btrieve.INDEX_1, record, 0)
+    #record = struct.pack(recordFormat,  0, 0, bytes(8), 0, 0, 0, 0)
+    identifier=5
+    record = struct.pack(recordFormat, identifier, 0, bytes(8), 0, 0, 0, 0)
+    rc = btrieveFile.KeyRetrieve(btrv.Btrieve.COMPARISON_EQUAL, btrv.Btrieve.INDEX_1, record)
+    assert(rc == btrv.Btrieve.STATUS_CODE_NO_ERROR)
+    #readLength = btrieveFile.RecordRetrieveLast(btrv.Btrieve.INDEX_1, record, 0)
     unpacked_record = struct.unpack(recordFormat, record)
-    #print(unpacked_record)
+
+    maxBlobSize = 1024 * 1024
+    blob = bytes(maxBlobSize)
+    rc = btrieveFile.RecordRetrieveChunk(recordLength, maxBlobSize, blob)
+    assert(rc >= 0)
+    
+    bytesob = io.BytesIO(blob)
+    im = Image.open(bytesob)
+    #print(im.format)
+    #print(im.size)
+    #print(im.mode)
+    im.save('DONT_NEED_BYTES.jpg', 'JPEG')
     return (unpacked_record)
 
-def insertRecord(imglocation, lat, lng, hour, minute, sec):
-    time = datetime.now()
-    print(time)
-    record = struct.pack(recordFormat, 0, 0, imglocation.ljust(50).encode('UTF-8'), 0, lat, 0, lng, 0, 0, hour, minute, sec)
+
+def insertRecord(imglocation, lat, lng):
+    #time = datetime.now()
+    #print(time)
+    record = struct.pack(recordFormat, 0, 0, bytes(8), 0, lat, 0, lng)
     #PCC interprets the 4bytes of the TIME datayte as PM/AM, sec, min, hour
     rc = btrieveFile.RecordCreate(record)
     if (rc == btrv.Btrieve.STATUS_CODE_NO_ERROR):
          print(' Insert successful!')
     else:
          print(' Insert failed - status: ', rc)
+    # Now we want to append the image data
+    realPath = os.path.realpath(imglocation)
+    print(realPath)
+    blobFile = open(realPath, mode='rb')
+    blob = blobFile.read()
+    blobFile.close()
+    rc = btrieveFile.RecordAppendChunk(blob)
+    if (rc == btrv.Btrieve.STATUS_CODE_NO_ERROR):
+         print(' Append Chunk successful!')
+    else:
+         print(' Append failed - status: ', rc)
+         #closeTable()
+         #break
+    #closeTable()
+    
 
 
 def closeTable():
@@ -92,11 +126,11 @@ def closeTable():
 
 
 if __name__ == '__main__':
-    #insertRecord(345, 678)
-    Data = select_all()
-    print(Data)
-    #lastRow = get_last()
-    #print(lastRow)
+    insertRecord('../javascriptUI/heroImages/m1.jpg', 345, 678)
+    #Data = select_all()
+    #print(Data)
+    lastRow = get_last()
+    print(lastRow)
     closeTable()
 
 
